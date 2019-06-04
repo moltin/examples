ade1fbdcf2d73eef2cf55addff3daa9c6cb7929b
Matthew Foyle <matt@moltin.com>
2019-06-04 13:48:50 +0100

    Update Shipstation util

--- sync-orders-with-shipstation/utils/shipstation.ts
+++ sync-orders-with-shipstation/utils/shipstation.ts
@@ -4,44 +4,50 @@ const buff = Buffer.from(userpass)
 const base64auth = buff.toString('base64')
 const request = require('request')
 
-module.exports.createShipstationOrder = requestBody => new Promise((resolve, reject) => {
-  const options = {
-    uri: 'https://ssapi.shipstation.com/orders/createorder',
-    headers: { Authorization: `Basic  ${base64auth}` },
-    method: 'POST',
-    json: requestBody,
-  }
+export const createShipstationOrder = (requestBody): Promise<any> => {
+    return new Promise((resolve, reject): any => {
+  
+        const options = {
+            uri: 'https://ssapi.shipstation.com/orders/createorder',
+            headers: { Authorization: `Basic  ${base64auth}` },
+            method: 'POST',
+            json: requestBody,
+        }
 
-  request(options, (error, response, responseBody) => {
-    if (error) {
-      reject(error)
-    }
+        request(options, (error, response, responseBody): any => {
+            if (error) {
+                reject(error)
+            }
 
-    if(response.statusCode !== 200 || 201) {
-      reject(responseBody)
-    }
+            if(response.statusCode !== 200 || 201) {
+                reject(responseBody)
+            }
 
-    resolve(responseBody)
-  })
-})
+            resolve(responseBody)
+        })
 
-module.exports.getShipStationWebhookResourceUrl = url => new Promise((resolve, reject) => {
-  const options = {
-    uri: url,
-    headers: { Authorization: `Basic  ${base64auth}` },
-    method: 'GET',
-  }
+    })
+}
 
-  request(options, (error, response, body) => {
+export const getShipStationWebhookResource = (url): Promise<any> => {
+    return new Promise((resolve, reject): any => {
+        const options = {
+            uri: url,
+            headers: { Authorization: `Basic  ${base64auth}` },
+            method: 'GET',
+        }
 
-    if (error) {
-      reject(error)
-    }
+        request(options, (error, response, body): any => {
 
-    if(response.statusCode !== 200) {
-      reject(body)
-    }
-    
-    resolve(body)
-  })
-})
+            if (error) {
+                return reject(error)
+            }
+
+            if(response.statusCode !== 200) {
+                return reject(body)
+            }
+        
+            return resolve(body)
+        })
+    })
+}
