3035f2749e6d6c4b2a22e4501803b84b35bdd14a
Matthew Foyle <matt@moltin.com>
2019-06-04 13:49:14 +0100

    Update handler

--- sync-orders-with-shipstation/handler.ts
+++ sync-orders-with-shipstation/handler.ts
@@ -1,60 +1,77 @@
 'use strict'
 import 'source-map-support/register'
-const transforms = require('./utils/transforms')
-const models = require('./models')
-const shipstation = require('./utils/shipstation')
-const moltin = require('./utils/moltin')
 
-const responseBuilder = code => message => ({
-  statusCode: code,
-  body: JSON.stringify({
-    message,
-  }),
-})
+import { Handler, APIGatewayEvent } from 'aws-lambda';
+import * as transforms from './utils/transforms'
+import * as shipstation from './utils/shipstation'
+import * as moltin from './utils/moltin'
+import * as models from './models'
+
+import {
+    LambdaEventBody,
+    LambdaResponse,
+    MoltinOrderPaidWebhookBody,
+    ShipstationOrderShippedWebhookBody,
+    ShipstationOrderValues,
+    ShipstationFullOrder,
+    ShipstationOrderObjects
+} from "Types"
+
+const responseBuilder = (code: number): Function => (message: string): LambdaResponse => {
+    return {
+        statusCode: code,
+        body: JSON.stringify({
+            message,
+        }),
+    }
+}
 
 /*
 newMoltinOrder takes a Moltin webhook payload for order.paid
 and sends the order to Shipstation
 */
-module.exports.newMoltinOrder = async (event) => {
-  const eventBody = JSON.parse(event.body)
-  const moltinOrderBody = JSON.parse(eventBody.resources).data
-  const { items } = JSON.parse(eventBody.resources).included
+export const newMoltinOrder: Handler = async (event: APIGatewayEvent): Promise<LambdaResponse> => {
+    try {
+        if (event.body === null) {
+            return responseBuilder(500)('No webhook payload present')
+        }
+        const eventBody: LambdaEventBody = JSON.parse(event.body as string)
+        const moltinOrderBody: MoltinOrderPaidWebhookBody = JSON.parse(eventBody.resources).data
+        const { items } = JSON.parse(eventBody.resources).included
 
-  const transformedLines = await transforms.transformLineItems(items)(models.orderItemModel)
-  const transformedShipping = transforms.transformShippingAddress(moltinOrderBody.shipping_address)(models.billingAddressModel)
-  const transformedBilling = transforms.transformBillingAddress(moltinOrderBody.billing_address)(models.billingAddressModel)
-
-  const orderBody = transforms.transformBaseOrder(moltinOrderBody)(models.orderModel)
-  orderBody.billTo = transformedBilling
-  orderBody.shipTo = transformedShipping
-  orderBody.items = transformedLines
-
-  try {
-    await shipstation.createShipstationOrder(orderBody)
-    return responseBuilder(200)('Order created in Shipstation')
-  } catch (e) {
-    console.log(e)
-    return responseBuilder(500)(JSON.stringify(e))
-  }
+        const orderObjects: ShipstationOrderObjects = await transforms.buildOrderObjects(items)(moltinOrderBody.shipping_address)(moltinOrderBody.billing_address)
+        const orderValues: ShipstationOrderValues = await transforms.buildOrderValues(moltinOrderBody)(models.orderModel)
+        const builtOrder: ShipstationFullOrder = transforms.buildFullOrder(orderObjects)(orderValues)
+    
+        await shipstation.createShipstationOrder(builtOrder)
+        return responseBuilder(200)('Order created in Shipstation')
+    } catch (e) {
+        return responseBuilder(500)(JSON.stringify(e))
+    }
 }
 
 /*
 newShipstationShipment takes a Shipstation webhook payload for SHIP_NOTIFY
 and sends marks the order in Moltin as fulfilled
 */
-module.exports.newShipstationShipment = async (event) => {
-  try {
-    // Shipstation webhooks provide a URL to fetch the changed resource
-    const resourceUrl = event.body.resource_url
-    const shipmentInfo = await shipstation.getShipStationWebhookResourceUrl(resourceUrl)
-    
-    const parsedShipmentInfo = JSON.parse(shipmentInfo)
-    const orderID = parsedShipmentInfo.shipments[0].orderKey
+export const newShipstationShipment: Handler = async (event: APIGatewayEvent): Promise<LambdaResponse> => {
+    try {
+        if (event.body === null) {
+            return responseBuilder(500)('No webhook payload present')
+        }
+        const { resource_url, resource_type }: ShipstationOrderShippedWebhookBody = JSON.parse(event.body as string)
+        
+        if(resource_type !== 'SHIP_NOTIFY') {
+            return responseBuilder(500)('Webhook is not notifying of shipment')
+        }
 
-    await moltin.markOrderAsShipped(orderID)
-    return responseBuilder(200)('Order marked as fulfilled in Moltin')
-  } catch (e) {
-    return responseBuilder(500)(JSON.stringify(e))
-  }
+        const shipmentInfo = await shipstation.getShipStationWebhookResource(resource_url)
+        const parsedShipmentInfo = JSON.parse(shipmentInfo)
+        const { orderKey } = parsedShipmentInfo.shipments[0]
+
+        await moltin.markOrderAsShipped(orderKey)
+        return responseBuilder(200)('Order marked as fulfilled in Moltin')
+    } catch (e) {
+        return responseBuilder(500)(JSON.stringify(e))
+    }
 }
