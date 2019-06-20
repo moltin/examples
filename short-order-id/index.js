6e5f226a29d3c510e0a3f8a38ac9c43fca226d07
Artyom Resh <artyom.resh@csssr.com>
2019-06-20 20:11:12 +0300

    fix(short-order-id): update example

--- short-order-id/index.js
+++ short-order-id/index.js
@@ -1,34 +1,27 @@
 const { json, send } = require('micro')
 const cors = require('micro-cors')()
-const { createClient } = require('@moltin/request')
+import { gateway } from '@moltin/sdk'
 const cuid = require('cuid')
 
-const moltin = new createClient({
-  client_id: process.env.MOLTIN_CLIENT_ID,
-  client_secret: MOLTIN_CLIENT_SECRET,
-  application: 'example-short-order-id'
+const moltin = gateway({
+  client_id: clientId,
+  client_secret: clientSecret,
 })
 
 module.exports = cors(async (req, res) => {
-  if (
-    (await req.headers['x-moltin-secret-key']) !=
-    process.env.MOLTIN_WEBHOOK_SECRET
-  )
+  if (req.headers['x-moltin-secret-key']) !== process.env.MOLTIN_WEBHOOK_SECRET) {
     return send(res, 401)
+  }
 
-  const payload = await json(req)
-
-  const {
-    data: { id }
-  } = JSON.parse(payload.resources)
+  const { body } = req
+  const { data: { id } } = JSON.parse(body.resources)
 
   try {
+    // The slug() here is just an example.
+    // It is not recommended to use it to generate id entities in the database.
     const short_id = cuid.slug().toUpperCase()
 
-    await moltin.put(`orders/${id}`, {
-      type: 'order',
-      short_id
-    })
+    await moltin.Orders.Update(id, { type: 'order', short_id })
 
     send(res, 200)
   } catch (errors) {
